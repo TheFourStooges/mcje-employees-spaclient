130a94b08489169b5eaccaf6080fb94a77eefd6c
pcminh <pcminh@users.noreply.github.com>
2021-12-23 03:32:03 +0700

    Order.component wrap with container

--- src/pages/order/order.component.jsx
+++ src/pages/order/order.component.jsx
@@ -1,13 +1,32 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useState, useEffect } from 'react';
+import { Container } from '@mui/material';
+import { useParams } from 'react-router-dom';
+import OrderView from '../../components/order-view/order-view.component';
+import config from '../../config/config';
 
-const OrderPage = () => {
+const OrderPage = (props) => {
   const { orderId } = useParams();
-  return (
-    <div>
-      Order {orderId}
-    </div>
-  )
-}
+  // const [order, setOrder] = useState(null);
+  
+  // useEffect(() => {
+  //   console.log('useEffect');
+  //   // async function fetchData() {
+  //   //   const resp = await axios.request({
+  //   //     baseURL: config.baseUrl,
+  //   //     url: `/order/${orderId}`,
+  //   //     method: 'GET',
+  //   //   });
+  //   //   setOrder(resp);
+  //   // }
 
-export default OrderPage
+  //   // fetchData();
+  // }, []);
+
+  return (
+    <Container maxWidth="xl">
+      <OrderView orderId={orderId} />
+    </Container>
+  );
+};
+
+export default OrderPage;
