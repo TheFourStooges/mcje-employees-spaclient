493f76f0b6e2183df5f92310be572dfc96c25835
pcminh <pcminh@users.noreply.github.com>
2021-12-23 03:28:17 +0700

    Add form-components, compatible with react-hook-form

---
+++ src/components/form-components/FormInputDropdown.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { FormControl, InputLabel, MenuItem, Select } from '@mui/material';
+import { useFormContext, Controller } from 'react-hook-form';
+
+// const options = [
+//   {
+//     label: "Dropdown Option 1",
+//     value: "1",
+//   },
+//   {
+//     label: "Dropdown Option 2",
+//     value: "2",
+//   },
+// ];
+
+const FormInputDropdown = ({ name, control, label, options }) => {
+  const generateSingleOptions = () => {
+    return options.map((option) => {
+      return (
+        <MenuItem key={option.value} value={option.value}>
+          {option.label}
+        </MenuItem>
+      );
+    });
+  };
+
+  return (
+    <FormControl size={'small'}>
+      <InputLabel>{label}</InputLabel>
+      <Controller
+        render={({ field: { onChange, value } }) => (
+          <Select onChange={onChange} value={value}>
+            {generateSingleOptions()}
+          </Select>
+        )}
+        control={control}
+        name={name}
+      />
+    </FormControl>
+  );
+};
+
+export default FormInputDropdown;
