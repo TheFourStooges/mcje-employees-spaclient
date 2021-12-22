41a9b2a79b6d6dbea22d8074bd6df539cf0e013b
pcminh <pcminh@users.noreply.github.com>
2021-12-23 03:30:12 +0700

    Asset page wrap with Container and Paper

--- src/pages/asset/asset.component.jsx
+++ src/pages/asset/asset.component.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import Paper from '@mui/material/Paper';
+import Container from '@mui/material/Container';
 import AssetForm from '../../components/asset-form/asset-form.component';
 
 const AssetPage = () => {
   const { assetId } = useParams();
 
   return (
-    <>
-      <AssetForm id={assetId} />
-    </>
+    <Container maxWidth="lg">
+      <Paper>
+        <AssetForm id={assetId} />
+      </Paper>
+    </Container>
   );
-}
+};
 
-export default AssetPage
+export default AssetPage;
