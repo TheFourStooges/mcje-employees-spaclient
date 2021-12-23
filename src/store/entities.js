cfd56fb5890f5ce0ce107a3ad0d14f1a3fc5ca06
pcminh <pcminh@users.noreply.github.com>
2021-12-23 16:51:44 +0700

    store: add shippingMethods + users

--- src/store/entities.js
+++ src/store/entities.js
@@ -3,10 +3,14 @@ import productsReducer from './products';
 import categoriesReducer from './categories';
 import assetsReducer from './assets';
 import ordersReducer from './orders';
+import usersReducer from './users';
+import shippingMethodsReducer from './shippingMethods';
 
 export default combineReducers({
   products: productsReducer,
   categories: categoriesReducer,
   assets: assetsReducer,
   orders: ordersReducer,
+  users: usersReducer,
+  shippingMethods: shippingMethodsReducer,
 });
